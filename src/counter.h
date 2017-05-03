ed42bbde1941d4121a1095d35e056a1c33cd0775
Vasil Zlatanov <v@skozl.com>
2017-05-03 22:48:25 +0100

    Add counter class for inputs.

---
+++ src/counter.h
@@ -0,0 +1,20 @@
+#include "mbed.h"
+
+class Counter {
+public:
+    Counter(PinName pin) : _interrupt(pin) {        // create the InterruptIn on the pin specified to Counter
+        _interrupt.rise(this, &Counter::increment); // attach increment function of this counter instance
+    }
+ 
+    void increment() {
+        _count++;
+    }
+ 
+    int read() {
+        return _count;
+    }
+ 
+private:
+    InterruptIn _interrupt;
+    volatile int _count;
+};
